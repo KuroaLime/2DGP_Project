9fd4117dbab6eff8299ddd244d70d0027cc04442
KuroaLime <benevolent0518@gmail.com>
2022-10-15 02:29:29 +0900

    first_stage's background Add

---
+++ Project01/first_stage.py
@@ -0,0 +1,12 @@
+from pico2d import *
+
+
+
+class first_stage:
+    def __init__(self):
+        self.image = load_image('Resource/stage/stage01/first_stage_sheet.png')
+        self.image = self.image.clip_image(804,0,677,233)
+
+    def draw(self):
+        self.image.draw(960,540,1920,1080)
+
