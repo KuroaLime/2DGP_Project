4b65692d3d5a995d226aeece631b59c2ccf662f9
KuroaLime <benevolent0518@gmail.com>
2022-10-15 23:10:23 +0900

    Player normal attack Add

---
+++ Project01/Plable_Kyoko.py
@@ -0,0 +1,100 @@
+from pico2d import *
+import game_framework
+
+# class Grass:
+#     def __init__(self):
+#         self.image = load_image('grass.png')
+#
+#     def draw(self):
+#         self.image.draw(400, 30)
+
+class kyoko:
+    def __init__(self):
+        self.x, self.y = 500, 500
+        self.frame = 0
+        self.last_frame=[12,    #idle
+                         12,    #walk
+                         16,     #run
+                         6,7,12     # 첫 어택: 6,  두번째 어택: 7,      세번째 어택: 12
+                         ]
+        self.frame_turn=0   #0:idle 1:walk  2:run   3: 첫 어택 4:두번째 어택    5:세번째 어택
+        self.dir_lr= 0 #오른쪽 왼쪽
+        self.dir_ud =0 # 위 아래
+        self.dir_last =1 #오른쪽 왼쪽 전 상태
+        self.run_state=0 #Run 상태
+        self.normal_attack_stack=0 #일반공격 콤보 스택 0:스냅 1:옆차기 2: 돌려차기
+        self.normal_attack_time=0
+        self.image = load_image('Player_kyoko.png')
+        self.image_L = load_image('Player_kyoko_L.png')
+        self.j_keyup=0
+    def update(self):
+        self.frame = (self.frame + 1) % self.last_frame[self.frame_turn]
+        self.normal_attack_time += 0.1
+        if self.run_state ==0:
+            self.x += self.dir_lr * 5
+            self.y += self.dir_ud * 5
+        else:
+            self.x += self.dir_lr * 10
+            self.y += self.dir_ud * 10
+
+        if self.normal_attack_time >=1.0:
+            self.normal_attack_stack=0
+        delay(0.05)
+    def run_dir_R(self):
+        if self.run_state > 0:
+            self.image.clip_draw(self.frame * 59, 134, 58, 70, self.x, self.y)
+        else:
+            self.image.clip_draw(self.frame * 40, 0, 40, 70, self.x, self.y)
+
+    def run_dir_L(self):
+        if self.run_state < 0:
+            self.image_L.clip_draw(self.frame * 62, 136, 58, 70, self.x, self.y)
+        else:
+            self.image_L.clip_draw(self.frame * 45, 69, 41, 68, self.x, self.y)
+    def normal_attack_changer(self):
+        if self.last_frame[self.frame_turn] - 1 == self.frame:
+            self.frame = 0
+            self.frame_turn = 0
+            if self.j_keyup >= 1:
+                self.j_keyup =0
+                self.normal_attack_stack = self.normal_attack_stack + 1
+                if self.normal_attack_stack >= 3:
+                    self.normal_attack_stack = 0
+    def draw(self):
+        if self.frame_turn <= 2:    #캐릭터 이동 그리기
+            if self.dir_lr == 1 and self.dir_ud == 0: # 오른쪽 이동
+                self.run_dir_R()
+            elif self.dir_lr == -1 and self.dir_ud == 0:    #왼쪽 이동
+                self.run_dir_L()
+            elif self.dir_lr == 0 and self.dir_ud == 0:   #idle
+                if self.dir_last == 1:   #오른쪽을 보는 상태
+                    self.image.clip_draw(self.frame * 39, 415, 39, 69, self.x, self.y)
+                else:               #왼쪽을 보는 상태
+                    self.image_L.clip_draw(self.frame * 41, 0, 37, 69, self.x, self.y)
+            elif self.dir_ud == 1:    #위쪽 이동
+                if self.dir_last > 0:
+                    self.run_dir_R()
+                else:
+                    self.run_dir_L()
+            elif self.dir_ud == -1:    #아래쪽 이동
+                if self.dir_last > 0:
+                    self.run_dir_R()
+                else:
+                    self.run_dir_L()
+        elif self.frame_turn <=5:
+            if self.normal_attack_stack <= 0:
+                if self.dir_last > 0:
+                    self.image.clip_draw(self.frame * 65, 484, 60, 67, self.x, self.y)
+                else:
+                    self.image_L.clip_draw(self.frame * 62, 210, 61, 65, self.x, self.y)
+            elif self.normal_attack_stack <= 1:
+                if self.dir_last > 0:
+                    self.image.clip_draw(self.frame * 69, 549, 68, 69, self.x, self.y)
+                else:
+                    self.image_L.clip_draw(self.frame * 70, 275, 70, 66, self.x, self.y)
+            elif self.normal_attack_stack <= 2:
+                if self.dir_last > 0:
+                    self.image.clip_draw(self.frame * 84, 620, 83, 72, self.x, self.y)
+                else:
+                    self.image_L.clip_draw(self.frame * 86, 341, 86, 69, self.x, self.y)
+            self.normal_attack_changer()
--- Project01/play_state.py
+++ Project01/play_state.py
@@ -7,72 +7,13 @@ import first_stage
 
 #적 임포트
 import enermy_class
-
+import Plable_Kyoko
 # class Grass:
 #     def __init__(self):
 #         self.image = load_image('grass.png')
 #
 #     def draw(self):
 #         self.image.draw(400, 30)
-
-class kyoko:
-    def __init__(self):
-        self.x, self.y = 500, 500
-        self.frame = 0
-        self.last_frame=[12,    #idle
-                         12,    #walk
-                         16     #run
-                         ]
-        self.frame_turn=0   #0:idle 1:walk  2:run
-        self.dir_lr= 0 #오른쪽 왼쪽
-        self.dir_ud =0 # 위 아래
-        self.dir_last =1 #오른쪽 왼쪽 전 상태
-        self.run_state=0
-        self.image = load_image('Player_kyoko.png')
-        self.image_L = load_image('Player_kyoko_L.png')
-    def update(self):
-        self.frame = (self.frame + 1) % self.last_frame[self.frame_turn]
-
-        if self.run_state ==0:
-            self.x += self.dir_lr * 5
-            self.y += self.dir_ud * 5
-        else:
-            self.x += self.dir_lr * 10
-            self.y += self.dir_ud * 10
-    def run_dir_R(self):
-        if self.run_state > 0:
-            self.image.clip_draw(self.frame * 59, 134, 58, 70, self.x, self.y)
-        else:
-            self.image.clip_draw(self.frame * 40, 0, 40, 70, self.x, self.y)
-    def run_dir_L(self):
-        if self.run_state < 0:
-            self.image_L.clip_draw(self.frame * 62, 136, 58, 70, self.x, self.y)
-        else:
-            self.image_L.clip_draw(self.frame * 45, 69, 41, 68, self.x, self.y)
-    def draw(self):
-
-        if self.dir_lr == 1 and self.dir_ud == 0: # 오른쪽 이동
-            self.run_dir_R()
-        elif self.dir_lr == -1 and self.dir_ud == 0:    #왼쪽 이동
-            self.run_dir_L()
-        elif self.dir_lr == 0 and self.dir_ud == 0:   #idle
-            if self.dir_last == 1:   #오른쪽을 보는 상태
-                self.image.clip_draw(self.frame * 39, 415, 39, 69, self.x, self.y)
-            else:               #왼쪽을 보는 상태
-                self.image_L.clip_draw(self.frame * 41, 0, 37, 69, self.x, self.y)
-        elif self.dir_ud == 1:    #위쪽 이동
-            if self.dir_last > 0:
-                self.run_dir_R()
-            else:
-                self.run_dir_L()
-        elif self.dir_ud == -1:    #아래쪽 이동
-            if self.dir_last > 0:
-                self.run_dir_R()
-            else:
-                self.run_dir_L()
-
-
-
 def handle_events():
     global running
     global Player
@@ -86,36 +27,57 @@ def handle_events():
             if event.key == SDLK_SPACE: #메뉴창
                 game_framework.quit()
                 # game_framework.push_state(menu_state)
-            elif event.key == SDLK_a: # 왼쪽 이동
-                Player.dir_lr = -1
-                if Player.dir_last!=Player.dir_lr:
-                    Player.dir_last = Player.dir_lr
-                frame_turn_changer()
-            elif event.key == SDLK_d: # 오른쪽 이동
-                Player.dir_lr = 1
-                if Player.dir_last!=Player.dir_lr:
-                    Player.dir_last = Player.dir_lr
-                frame_turn_changer
-            elif event.key == SDLK_w: # 위쪽 이동
-                Player.dir_ud = 1
-                if Player.dir_last!=Player.dir_lr:
-                    Player.dir_last = Player.dir_lr
-                frame_turn_changer
-            elif event.key == SDLK_s: # 아래쪽 이동
-                Player.dir_ud = -1
-                if Player.dir_last!=Player.dir_lr:
-                    Player.dir_last = Player.dir_lr
-                frame_turn_changer
-            elif event.key == SDLK_TAB:
-                if Player.dir_lr == 1:
-                    Player.run_state = 1
-                elif Player.dir_lr == -1:
-                    Player.run_state = -1
-                elif Player.dir_ud == 1 or Player.dir_ud == -1:
-                    if Player.dir_last == 1:
+
+            elif event.key == SDLK_j:
+                #공격 상태
+                if Player.frame_turn<=0:
+                    if Player.normal_attack_stack == 0:
+                        Player.frame=0
+                        Player.frame_turn = 3
+                    elif Player.normal_attack_stack ==1:
+                        Player.frame = 0
+                        Player.frame_turn =4
+                    elif Player.normal_attack_stack ==2:
+                        Player.frame = 0
+                        Player.frame_turn =5
+                    Player.normal_attack_time=0
+                    #초기화
+                    Player.dir_lr =0
+                    Player.dir_ud=0
+                    Player.run_state=0
+
+            elif Player.frame_turn <=2:
+                if event.key == SDLK_a: # 왼쪽 이동
+                    Player.dir_lr = -1
+                    if Player.dir_last!=Player.dir_lr:
+                        Player.dir_last = Player.dir_lr
+                    frame_turn_changer()
+                elif event.key == SDLK_d: # 오른쪽 이동
+                    Player.dir_lr = 1
+                    if Player.dir_last!=Player.dir_lr:
+                        Player.dir_last = Player.dir_lr
+                    frame_turn_changer
+                elif event.key == SDLK_w: # 위쪽 이동
+                    Player.dir_ud = 1
+                    if Player.dir_last!=Player.dir_lr:
+                        Player.dir_last = Player.dir_lr
+                    frame_turn_changer
+                elif event.key == SDLK_s: # 아래쪽 이동
+                    Player.dir_ud = -1
+                    if Player.dir_last!=Player.dir_lr:
+                        Player.dir_last = Player.dir_lr
+                    frame_turn_changer
+                elif event.key == SDLK_TAB:
+                    if Player.dir_lr == 1:
                         Player.run_state = 1
-                    elif Player.dir_last == -1:
+                    elif Player.dir_lr == -1:
                         Player.run_state = -1
+                    elif Player.dir_ud == 1 or Player.dir_ud == -1:
+                        if Player.dir_last == 1:
+                            Player.run_state = 1
+                        elif Player.dir_last == -1:
+                            Player.run_state = -1
+
         elif event.type == SDL_KEYUP:
             if event.key == SDLK_a or event.key == SDLK_d:
                 Player.dir_lr = 0
@@ -127,6 +89,8 @@ def handle_events():
                 frame_turn_changer()
             elif event.key == SDLK_TAB:
                 Player.run_state=0
+            elif event.key == SDLK_j:
+                Player.j_keyup=1
 
 
 
@@ -154,7 +118,7 @@ def enter():
 
     hide_cursor()
 
-    Player = kyoko()
+    Player = Plable_Kyoko.kyoko()
     Enermy = [enermy_class.cheerleader()]
 
     background=first_stage.first_stage()
@@ -171,18 +135,20 @@ def exit():
 
 def update():
     Player.update()
-    for i in range(0, len(Enermy)):
-        Enermy[i].update()
+    # for i in range(0, len(Enermy)):
+    #     Enermy[i].update()
 
 def draw():
     clear_canvas()
     draw_world()
+    # draw_enermy()
     update_canvas()
 
 
 def draw_world():
     background.draw()
     Player.draw()
+def draw_enermy():
     for i in range(0,len(Enermy)):
         Enermy[i].draw()
 
