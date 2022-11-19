fdf25ab8a656169ab036fc6bdb727f6ac674dca5
KuroaLime <benevolent0518@gmail.com>
2022-11-19 22:10:24 +0900

    AI of Cheerleader Modify

--- Works/cheerleader.py
+++ Works/cheerleader.py
@@ -3,6 +3,7 @@ import game_framework
 import play_state
 import random
 import game_world
+from BehaviorTree import BehaviorTree, SelectorNode, SequenceNode, LeafNode
 
 PIXEL_PER_METER = (10.0 / 0.3)
 RUN_SPEED_KMPH = 10.0
@@ -15,80 +16,126 @@ ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
 FRAMES_PER_ACTION_IDLE = 12
 FRAMES_PER_ACTION_RUN = 12
 
+animation_names = ['Idle', 'Run']
 class Cheerleader:
-    image=None
+    images=None
+    def load_images(self):
+        if Cheerleader.images == None:
+            Cheerleader.images = {}
+            for name in animation_names:
+                Cheerleader.images[name] = [load_image("./Resource/classification/enermy/cheerleader/Occupied/"+ name + " (%d)" % i + ".png") for i in range(1, 13)]
+
+    def prepare_patrol_points(self):
+        positions = [(43, 750), (1118, 750), (1050, 530), (575, 220), (235, 33), (575,220), (1050, 530), (1118,750)]
+        self.patrol_points = []
+        for p in positions:
+            self.patrol_points.append((p[0], 1024-p[1]))
     def __init__(self):
+        self.prepare_patrol_points()
+        self.patrol_order = 1
         self.hp = 20
-        self.x, self.y, self.t= random.randint(400,500), random.randint(200,400),0.0
-        self.sx,self.sy=self.x, self.y
-        self.ax,self.ay = play_state.Player.x,play_state.Player.y
+        self.x, self.y= self.patrol_points[0]
         self.frame = 0
-        self.last_frame=[12,    #idle
-                         12,    #walk
-                         16     #run
-                         ]
-        self.frame_turn=0   #0:idle 1:walk
-        self.dir_lr= 0 #오른쪽 왼쪽
-        self.dir_ud =0 # 위 아래
-        self.dir_last =1 #오른쪽 왼쪽 전 상태
-        if Cheerleader.image == None:
-            Cheerleader.image = load_image('Character/enermy_cheerleader.png')
+        # self.last_frame=[12,    #idle
+        #                  12,    #walk
+        #                  16     #run
+        #                  ]
+        self.dir = random.random()*2*math.pi
+        self.speed = 0
+        self.timer = 1.0
+        self.wait_timer = 2.0
+        self.load_images()
+        self.build_behavior_tree()
         #self.image_L = load_image('Player_kyoko_L.png')
 
-    def arrival_point_changer(self):
-        if self.ax!=play_state.Player.x or self.ay!=play_state.Player.y:
-            # if self.t >= 1.0:
-            #     self.t = 0
-            self.sx, self.sy = self.x, self.y
-            self.ax, self.ay = play_state.Player.x, play_state.Player.y
+    def wander(self):
+        self.speed = RUN_SPEED_PPS
+        self.timer -= game_framework.frame_time
+        if self.timer <= 0:
+            self.timer = 1.0
+            self.dir = random.random() * 2 * math.pi
+            return BehaviorTree.SUCCESS
+    def wait(self):
+        self.speed = 0
+        self.wait_timer -= game_framework.frame_time
+        if self.wait_timer <=0:
+            self.wait_timer = 2.0
+            return BehaviorTree.SUCCESS
+        else:
+            return BehaviorTree.RUNNING
+
+    def find_player(self):
+        distance2 = (play_state.Player.x - self.x)**2 + (play_state.Player.y - self.y)**2
+        if distance2 <= (PIXEL_PER_METER*10)**2:
+            return BehaviorTree.SUCCESS
+        else:
+            self.speed = 0
+            return BehaviorTree.FAIL
+
+    def move_to_player(self):
+        self.speed = RUN_SPEED_PPS
+        self.dir = math.atan2(play_state.Player.y - self.y, play_state.Player.x - self.x)
+        return BehaviorTree.SUCCESS
+
+    def get_next_position(self):
+        self.target_x, self.target_y = self.patrol_points[self.patrol_order % len(self.patrol_points)]
+        self.patrol_order += 1
+        self.dir = math.atan2(self.target_y - self.y, self.target_x - self.x)
+        return BehaviorTree.SUCCESS
+
+    def move_to_target(self):
+        self.speed = RUN_SPEED_PPS
+        distance2 = (play_state.Player.x - self.x)**2 + (play_state.Player.y - self.y)**2
+        if distance2 <= PIXEL_PER_METER**2:
+            return BehaviorTree.SUCCESS
+        else:
+            return BehaviorTree.RUNNING
+
+    def build_behavior_tree(self):
+        wander_node = LeafNode('Wander', self.wander)
+        wait_node = LeafNode('Wait', self.wait)
+
+        wander_wait_node = SequenceNode('WanderAndWait')
+        wander_wait_node.add_children(wander_node, wait_node)
+
+        get_next_position_node = LeafNode('Get Next Position', self.get_next_position())
+        move_to_target_node = LeafNode('Move to Target', self.move_to_target)
+        patrol_node = SequenceNode('Patrol')
+        patrol_node.add_children(get_next_position_node, move_to_target_node)
+
+        find_player_node = LeafNode('Find Player', self.find_player)
+        move_to_player_node = LeafNode('Move to Target', self.move_to_player)
+        chase_node = SequenceNode('Chase')
+        chase_node.add_children(find_player_node, move_to_player_node)
+
+        chase_wander_node = SelectorNode('Chase or Wander')
+        chase_wander_node.add_children(chase_node, wander_node)
+
+        self.bt = BehaviorTree(chase_wander_node)
+
     def update(self):
         if self.hp <= 0:
             game_world.remove_object(self)
-        self.frame = (self.frame + FRAMES_PER_ACTION_RUN * ACTION_PER_TIME * game_framework.frame_time) % 12
-        #self.t += 0.0005
-        if self.x != self.ax:
-            if self.x > self.ax:
-                self.dir_lr= -1
-            elif self.x < self.ax:
-                self.dir_lr = 1
-        if self.y != self.ay:
-            if self.y > self.ay:
-                self.dir_ud = -1
-            elif self.y < self.ay:
-                self.dir_ud = 1
-        self.x+=self.dir_lr * RUN_SPEED_PPS * game_framework.frame_time
-        self.y+=self.dir_ud * RUN_SPEED_PPS * game_framework.frame_time
-        self.arrival_point_changer()
+        self.bt.run()
 
-    def frame_turn_changer(self):
-        if self.dir_lr != 0 or self.dir_ud != 0:
-            if self.frame_turn != 1:
-                self.frame_turn == 1
-        elif self.dir_lr == 0 and self.dir_ud == 0:
-            self.frame_turn == 0
+        self.frame = (self.frame + FRAMES_PER_ACTION_RUN * ACTION_PER_TIME * game_framework.frame_time)%FRAMES_PER_ACTION_RUN
+        self.x += self.speed * math.cos(self.dir) * game_framework.frame_time
+        self.y += self.speed * math.sin(self.dir) * game_framework.frame_time
+
     def draw(self):
         draw_rectangle(*self.get_bb())
         draw_rectangle(*self.get_TT())
 
-        if self.dir_lr > 0: # 오른쪽 이동
-            self.image.clip_composite_draw(int(self.frame) * 74, 0, 72, 73,0,'', self.x, self.y, 200,200)
-        elif self.dir_lr < 0:    #왼쪽 이동
-            self.image.clip_composite_draw(int(self.frame) * 74, 0, 72, 73,0,'h', self.x, self.y, 200,200)
-        elif self.dir_lr == 0 and self.dir_ud == 0:   #idle
-            if self.dir_last > 0:   #오른쪽을 보는 상태
-                self.image.clip_composite_draw(int(self.frame) * 90, 273, 80, 72,0,'', self.x, self.y, 200,200)
-            else:               #왼쪽을 보는 상태
-                self.image.clip_composite_draw(int(self.frame) * 90, 273, 80, 72,0,'h', self.x, self.y, 200,200)
-        elif self.dir_ud > 0:    #위쪽 이동
-            if self.dir_last >0:
-                self.image.clip_composite_draw(int(self.frame) * 74, 0, 72, 73,0,'', self.x, self.y, 200,200)
+        if math.cos(self.dir) < 0:
+            if self.speed == 0:
+                Cheerleader.images['Idle'][int(self.frame)].composite_draw(0, 'h', self.x, self.y, 200, 200)
             else:
-                self.image.clip_composite_draw(int(self.frame) * 74, 0, 72, 73,0,'h', self.x, self.y, 200,200)
-        elif self.dir_ud < 0:    #아래쪽 이동
-            if self.dir_last > 0:
-                self.image.clip_composite_draw(int(self.frame) * 74, 0, 72, 73,0,'', self.x, self.y, 200,200)
+                Cheerleader.images['Run'][int(self.frame)].composite_draw(0, 'h', self.x, self.y, 200, 200)
+        else:
+            if self.speed == 0:
+                Cheerleader.images['Idle'][int(self.frame)].draw(self.x, self.y, 200, 200)
             else:
-                self.image.clip_composite_draw(int(self.frame) * 74, 0, 72, 73,0,'h', self.x, self.y, 200,200)
+                Cheerleader.images['Run'][int(self.frame)].draw(self.x, self.y, 200, 200)
     def get_bb(self):   #적, 자판기등의 오브젝트와의 충돌범위
         return self.x - 60, self.y - 95, self.x + 60, self.y + 85
     def get_TT(self):   #스테이지와의 충돌
